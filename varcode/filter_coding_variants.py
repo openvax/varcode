637e5353f7538fb238244adb8547425a9f6d4ba8
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2014-11-14 20:14:12 -0500

    print all the variants and their transcripts

--- varcode/filter_coding_variants.py
+++ varcode/filter_coding_variants.py
@@ -1,6 +1,7 @@
 import argparse
 
 import vcf
+import pyensembl
 
 parser = argparse.ArgumentParser()
 
@@ -13,11 +14,23 @@ parser.add_argument("--output-vcf",
 parser.add_argument("--output-csv",
     help="CSV output with chr/pos/ref/alt from VCF and annotation")
 
+
 if __name__ == "__main__":
+    # TODO: determine ensembl release from VCF metadata
+    ensembl = pyensembl.EnsemblRelease(75)
+
     args = parser.parse_args()
     with open(args.input, 'r') as f:
         vcf_reader = vcf.Reader(f)
         for record in vcf_reader:
-            print record
-            for sample in record.samples:
-                print "  ", sample
+            chrom, pos = record.CHROM, record.POS
+            ref, alt = record.REF, record.ALT
+            gene_names =  ensembl.gene_names_at_locus(chrom, pos)
+            print chrom, "@", pos, " :: ", ref, "->", alt
+            for gene in gene_names:
+                print "  ", gene
+                # TODO: implement transcript_names_of_gene_name in pyensembl
+                # and transcript_name_of_transcript_id
+                transcript_ids = ensembl.transcript_ids_of_gene_name(gene)
+                for transcript_id in transcript_ids:
+                    print "    ", transcript_id
