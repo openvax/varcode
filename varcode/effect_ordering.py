f57e1f17e607bb013b60c4a46204302648d282d6
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2015-03-02 16:55:41 -0500

    added license headers and changed copyright years

--- varcode/effect_ordering.py
+++ varcode/effect_ordering.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2015. Mount Sinai School of Medicine
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from .effects import *
 
 transcript_effect_priority_list = [
@@ -81,6 +95,7 @@ def top_priority_transcript_effect(effects):
             for effect in best_effects
             if effect.transcript.complete
         ]
+
         def key_fn(effect):
             return len(effect.transcript.coding_sequence)
     else:
