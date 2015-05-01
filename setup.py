4ff5dd4f402467795f302319d797cc54b777dcb3
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2015-05-01 02:33:47 -0400

    export NonsilentCodingMutation

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     setup(
         name='varcode',
         packages=['varcode'],
-        version="0.3.5",
+        version="0.3.6",
         description="Variant annotation in Python",
         long_description=readme,
         url="https://github.com/hammerlab/varcode",
