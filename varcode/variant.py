fd4c45d6d8680f55154c9cc6772cd7057112893b
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2016-03-14 19:30:23 -0400

    got rid of Variant.preserves_reading_frame

--- varcode/variant.py
+++ varcode/variant.py
@@ -711,11 +711,3 @@ class Variant(object):
     def is_indel(self):
         """Is this variant either an insertion or deletion?"""
         return self.is_insertion or self.is_deletion
-
-    @property
-    def preserves_reading_frame(self):
-        """
-        If this variant occurs in the coding region of a gene, would it
-        prerseve the ORF or create a frameshift mutation?
-        """
-        return (len(self.alt) - len(self.ref)) % 3 == 0
