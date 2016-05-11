0051c1a2243a5763b5f7efa80bb35f77347b47bd
Arun Ahuja <aahuja11@gmail.com>
2016-05-11 14:37:50 -0400

    update lint script to ignore versioneer.py

--- lint.sh
+++ lint.sh
@@ -4,7 +4,7 @@ set -o errexit
 # getting false positives due to this issue with pylint:
 # https://bitbucket.org/logilab/pylint/issues/701/false-positives-with-not-an-iterable-and
 
-find . -name '*.py' \
+find varcode test -name '*.py' \
   | xargs pylint \
   --errors-only \
   --disable=print-statement,unsubscriptable-object,not-an-iterable
