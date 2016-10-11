a52ca1fb750fe998cc4cc4d6c4907883459e9429
Julia K <julia326@gmail.com>
2016-10-11 12:08:30 -0400

    Updating required pyensembl version

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ if __name__ == '__main__':
         install_requires=[
             'numpy>=1.7, <2.0',
             'pandas>=0.15',
-            'pyensembl>=1.0.0',
+            'pyensembl>=1.0.2',
             'biopython>=1.64',
             'pyvcf>=0.6.7',
             'memoized_property>=1.0.2',
