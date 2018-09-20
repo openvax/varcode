ff6062fe599080c17160edac663023f732326e32
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2018-09-20 10:06:00 -0400

    fixed frameshift test

--- test/test_mm10_klf6_frameshift.py
+++ test/test_mm10_klf6_frameshift.py
@@ -7,6 +7,7 @@ from varcode.effects.effect_prediction_coding_frameshift import (
 
 from nose.tools import eq_
 
+
 def validate_effect_values(effect):
     eq_(effect.__class__, FrameShift)
     transcript = effect.transcript
@@ -14,11 +15,13 @@ def validate_effect_values(effect):
     eq_(transcript.spliced_offset(5864876), 469)
     eq_(effect.shifted_sequence, "GEEGGIRTEDFF")
 
+
 def test_mm10_Klf6_frameshift():
     variant = Variant("chr13", 5864876, "", "G", "GRCm38")
     effects = variant.effects().drop_silent_and_noncoding()
     eq_(len(effects), 1)
     validate_effect_values(effects[0])
+
 
 def test_mm10_Klf6_frameshift_coding_effect_fn():
     variant = Variant("chr13", 5864876, "", "G", "GRCm38")
@@ -30,16 +33,17 @@ def test_mm10_Klf6_frameshift_coding_effect_fn():
     eq_(len(coding_transcripts), 1)
     t = coding_transcripts[0]
     eq_(t.name, "Klf6-201")
-    # first start codon offset is 150
-    # mutation occurs after offset 462
+    # first start codon offset is 157
+    # mutation occurs after offset 469
     effect = predict_frameshift_coding_effect(
         trimmed_cdna_ref="",
         trimmed_cdna_alt="G",
-        cds_offset=469 - 150,
-        sequence_from_start_codon=t.sequence[150:],
+        cds_offset=469 - 157,
+        sequence_from_start_codon=t.sequence[157:],
         variant=variant,
         transcript=t)
     validate_effect_values(effect)
+
 
 def test_mm10_Klf6_frameshift_cdna_codon_sequence():
     variant = Variant("chr13", 5864876, "", "G", "GRCm38")
@@ -53,8 +57,8 @@ def test_mm10_Klf6_frameshift_cdna_codon_sequence():
     eq_(t.name, "Klf6-201")
     mutant_codon_index, seq_after_mutated_codon = \
         cdna_codon_sequence_after_insertion_frameshift(
-            sequence_from_start_codon=t.sequence[150:],
-            cds_offset_before_insertion=469 - 150,
+            sequence_from_start_codon=t.sequence[157:],
+            cds_offset_before_insertion=469 - 157,
             inserted_nucleotides="G")
     eq_(mutant_codon_index, 104)
     expected_sequence = t.sequence[469] + "G" + t.sequence[470:]
