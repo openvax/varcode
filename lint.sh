5b18f613340886435096bc55fcccc1d7d9739b04
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2024-01-23 16:28:22 -0500

    rename test->tests, updated linting script

--- lint.sh
+++ lint.sh
@@ -4,9 +4,9 @@ set -o errexit
 # getting false positives due to this issue with pylint:
 # https://bitbucket.org/logilab/pylint/issues/701/false-positives-with-not-an-iterable-and
 
-find varcode test -name '*.py' \
+find varcode tests -name '*.py' \
   | xargs pylint \
   --errors-only \
-  --disable=print-statement,unsubscriptable-object,not-an-iterable
+  --disable=unsubscriptable-object,not-an-iterable
 
 echo 'Passes pylint check'
